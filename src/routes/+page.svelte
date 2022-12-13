cf069e5f0b461feba34d1e42e7b127e50c092642
Tyler Meekel <ty.meekel@gmail.com>
2022-12-13 00:58:22 -0700

    adds localStorage support

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,12 +5,21 @@
 	import TodoComponent from "../components/TodoComponent.svelte";
     import type { Board } from "../classes/Board";
 	import BoardComponent from "../components/BoardComponent.svelte";
+	import { browser } from "$app/environment";
 
     let boardsData: Array<Board>;
+
     let showCreateTodo:boolean;
 
-    boards.subscribe((d: Board[]) => {
-        boardsData = d;
+    if(browser){
+        if(localStorage.getItem('data')){
+            console.log('dataasdasd')
+            boards.set(JSON.parse(localStorage.getItem('data') || '{}'));
+        }
+    }
+
+    boards.subscribe((data: Board[]) => {
+        boardsData = data;
     })
 
     showCreateTodoState.subscribe(data => { //Pulls data from store to use for visibility of Todo dialogue box
@@ -20,6 +29,15 @@
     function toggleShowCreateTodo(): void { //Handles toggling for the visibility of the Todo add dialogue box
         showCreateTodoState.set(true)
     }
+
+    function save() {
+        if(browser){
+            let jsonData = JSON.stringify(boardsData)
+            localStorage.setItem('data', jsonData)
+        }
+        
+    }
+
 </script>
 
 {#if showCreateTodo == true}
@@ -27,8 +45,11 @@
 {/if}
 
 <div class="h-full w-screen flex flex-col">
-    <header class=" h-20 w-screen bg-gray-200 flex items-center p-1 drop-shadow-md shadow-stone-900">
+    <header class=" h-20 w-screen bg-gray-200 flex items-center p-1 drop-shadow-md shadow-stone-900 justify-between">
         <h1 class="text-3xl ml-2">Kan(t)ban</h1>
+        <button class="mr-2 bg-gray-600 p-2 rounded-lg text-gray-100" on:click={save}>
+            Save
+        </button>
     </header>
     <main class="w-screen bg-gray-200 flex-1 flex gap-8 px-8 pt-8 overflow-x-scroll">
         {#each boardsData as board}
